55ae16c4d9cd0e89e0b71110b63da7ef6eb9ee1a
DarielMalave <darielmalaveperez@gmail.com>
2022-01-18 18:23:41 -0600

    Further design on Javascript pagination practice

--- JavascriptStuff/script.js
+++ JavascriptStuff/script.js
@@ -1,4 +1,4 @@
-const list_items = [
+const data_source = [
 	"Item 1",
 	"Item 2",
 	"Item 3",
@@ -23,11 +23,12 @@ const list_items = [
 	"Item 22"
 ];
 
-const list_element = document.getElementById('list');
-const pagination_element = document.getElementById('pagination');
+const data_container = document.getElementById('list');
+const pagination_bar = document.getElementById('pagination');
 
 let current_page = 1;
-let rows = 5;
+let rows_per_page = 5;
+let number_of_pages = Math.ceil(data_source.length / rows_per_page);
 
 function displayList(items, wrapper, rows_per_page, page) {
   wrapper.innerHTML = "";
@@ -53,41 +54,36 @@ function displayList(items, wrapper, rows_per_page, page) {
   }
 }
 
-function setupPagination(items, wrapper, rows_per_page) {
-  wrapper.innerHTML = "";
-
-  // round up to include leftover items from data source
-  let page_count = Math.ceil(items.length / rows_per_page);
-  // create pagination buttons and append them to wrapper
-  for (let i = 1; i < page_count + 1; i++) {
-    let btn = paginationButton(i, items);
-    wrapper.appendChild(btn);
-  }
-}
-
-// create individual button elements so that setupPagination()
-// can combine buttons for the paginaton bar
-function paginationButton(page, items) {
+function nextButton(pagination_bar) {
   let button = document.createElement('button');
-  button.innerText = page;
+  button.innerText = "Next Page";
+  pagination_bar.appendChild(button);
 
-  if (current_page == page) {
-    button.classList.add('active');
-  }
-
-  // add event listeners to make buttons clickable
   button.addEventListener('click', function() {
-    current_page = page;
-    displayList(items, list_element, rows, current_page);
-
-    let current_btn = document.querySelector('.pagenumbers button.active');
-    current_btn.classList.remove('active');
-
-    button.classList.add('active');
+    let current_page = document.getElementById('current_page_counter');
+    // this conditional is to make sure paginated results do not go out of bounds
+    if (0 <= current_page.value && current_page.value < number_of_pages) {
+      current_page.value ++;
+      displayList(data_source, data_container, rows_per_page, current_page.value);
+    }
   });
-
-  return button;
 }
 
-displayList(list_items, list_element, rows, current_page);
-setupPagination(list_items, pagination_element, rows);
+function previousButton(pagination_bar) {
+  let button = document.createElement('button');
+  button.innerText = "Previous Page";
+  pagination_bar.appendChild(button);
+
+  button.addEventListener('click', function() {
+    let current_page = document.getElementById('current_page_counter');
+    // this conditional is to make sure paginated results do not go out of bounds
+    if (1 < current_page.value && current_page.value <= number_of_pages) {
+      current_page.value --;
+      displayList(data_source, data_container, rows_per_page, current_page.value);
+    }
+  });
+}
+
+displayList(data_source, data_container, rows_per_page, current_page);
+previousButton(pagination_bar);
+nextButton(pagination_bar);

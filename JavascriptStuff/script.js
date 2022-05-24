d4b4476a31972ab3eb3d210f084049a6f245094a
DarielMalave <darielmalaveperez@gmail.com>
2022-05-23 20:21:29 -0500

    JavaScript practice from Udemy course - reviewing JavaScript variables and data types (going over basics)

--- JavascriptStuff/script.js
+++ JavascriptStuff/script.js
@@ -1,103 +1,15 @@
-const data_source = [
-	"Item 1",
-	"Item 2",
-	"Item 3",
-	"Item 4",
-	"Item 5",
-	"Item 6",
-	"Item 7",
-	"Item 8",
-	"Item 9",
-	"Item 10",
-	"Item 11",
-	"Item 12",
-	"Item 13",
-	"Item 14",
-	"Item 15",
-	"Item 16",
-	"Item 17",
-	"Item 18",
-	"Item 19",
-	"Item 20",
-	"Item 21",
-	"Item 22"
-];
+let country = "United States";
+let continent = "North America";
+let population = "330 million";
 
-const data_container = document.getElementById('list');
-const pagination_bar = document.getElementById('pagination');
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
-let current_page = 1;
-let rows_per_page = 5;
-let number_of_pages = Math.ceil(data_source.length / rows_per_page);
+let isIsland = false;
+let langauge;
 
-function displayList(items, wrapper, rows_per_page, page) {
-  wrapper.innerHTML = "";
-  // this is done because array indexes start at zero
-  page --;
-
-  // start acts as an offset
-  let start = rows_per_page * page;
-  // end simply adds the offset to how many rows are in a page
-  let end = start + rows_per_page;
-  // carve out the specific items needed from data source
-  let paginatedItems = items.slice(start, end);
-  // iterate through spliced data source and display the data
-  // in a wrapper containter
-  for (let i = 0; i < paginatedItems.length; i++) {
-    let item = paginatedItems[i];
-
-    let item_element = document.createElement('div');
-    item_element.classList.add('item');
-    item_element.innerText = item;
-
-    wrapper.appendChild(item_element);
-  }
-}
-
-function nextButton(pagination_bar) {
-  let button = document.createElement('button');
-  let page_display = document.getElementById('current_page_display');
-  button.innerText = "Next Page";
-  pagination_bar.appendChild(button);
-
-  button.addEventListener('click', function() {
-    let current_page = document.getElementById('current_page_counter');
-    // this conditional is to make sure paginated results do not go out of bounds
-    if (0 <= current_page.value && current_page.value < number_of_pages) {
-      current_page.value ++;
-      displayList(data_source, data_container, rows_per_page, current_page.value);
-      page_display.innerText = current_page.value;
-    }
-  });
-}
-
-function previousButton(pagination_bar) {
-  let button = document.createElement('button');
-  let page_display = document.getElementById('current_page_display');
-  button.innerText = "Previous Page";
-  pagination_bar.appendChild(button);
-
-  button.addEventListener('click', function() {
-    let current_page = document.getElementById('current_page_counter');
-    // this conditional is to make sure paginated results do not go out of bounds
-    if (1 < current_page.value && current_page.value <= number_of_pages) {
-      current_page.value --;
-      displayList(data_source, data_container, rows_per_page, current_page.value);
-      page_display.innerText = current_page.value;
-    }
-  });
-}
-
-// function pageCounter(pagination_bar, current_page) {
-//   let button = document.createElement('button');
-//   //let current_page_counter = document.getElementById('current_page_counter');
-//   button.id = 'current_page_count';
-//   button.innerText = current_page;
-
-//   pagination_bar.appendChild(button);
-// }
-
-displayList(data_source, data_container, rows_per_page, current_page);
-previousButton(pagination_bar);
-nextButton(pagination_bar);
-// pageCounter(pagination_bar, 1);
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(langauge);
